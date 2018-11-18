9a270a64685f6c3a95007760d7859bbba792e8be
leskovitaliy <lesko.vitaliy90@gmail.com>
2018-11-18 22:09:16 +0200

    add all routes

--- routes/auth.js
+++ routes/auth.js
@@ -2,9 +2,7 @@ const express = require('express');
 const controller = require('../controllers/auth');
 const router = express.Router();
 
-// localhost:5000/api/auth/login
-router.get('/login', controller.login);
-// localhost:5000/api/auth/register
-router.get('/register', controller.register);
+router.post('/login', controller.login);
+router.post('/register', controller.register);
 
 module.exports = router;
