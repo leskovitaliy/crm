046f149673ff727135b1d62392c14676310c007f
leskovitaliy <lesko.vitaliy90@gmail.com>
2018-11-19 20:18:52 +0200

    connect mLab

--- app.js
+++ app.js
@@ -1,4 +1,5 @@
 const express = require('express');
+const mongoose = require('mongoose');
 const bodyParser = require('body-parser');
 const cors = require('cors');
 const morgan = require('morgan');
@@ -7,12 +8,19 @@ const analyticsRoutes = require('./routes/analytics');
 const categoryRoutes = require('./routes/category');
 const orderRoutes = require('./routes/order');
 const positionRoutes = require('./routes/position');
+const keys = require('./config/keys');
 const app = express();
+
+mongoose.connect(keys.mongoURL, { useNewUrlParser: true })
+    .then(() => console.log('Mongo db connected.'))
+    .catch(err => console.log(err));
 
 app.use(morgan('dev')); // for log api in terminal
 
 app.use(bodyParser.urlencoded({ extended: true })); // parse response
 app.use(bodyParser.json());
+
+app.use(cors());
 
 app.use('/api/auth', authRoutes);
 app.use('/api/analytics', analyticsRoutes);
---
+++ config/keys.js
@@ -0,0 +1,3 @@
+module.exports = {
+  mongoURL: 'mongodb://lesko.vitaliy:qwest9384402@ds247141.mlab.com:47141/crm'
+};
