513ad140faee0bbf0fb5967e67c83e76b578ff01
leskovitaliy <lesko.vitaliy90@gmail.com>
2018-11-24 16:29:57 +0200

    add logic in controllers for category and position

--- controllers/category.js
+++ controllers/category.js
@@ -1,19 +1,51 @@
-module.exports.getAll = (req, res) => {
-  res.json({ message: 'CategoryId' })
+const Category = require('../models/Category');
+const Position = require('../models/Position');
+const errorHandler = require('../utils/errorHandler');
+
+module.exports.getAll = async (req, res) => {
+  try {
+    const categories = await Category.find({ user: req.user.id });
+    res.status(200).json(categories);
+  } catch (e) {
+    errorHandler(e);
+  }
 };
 
-module.exports.getById = (req, res) => {
-
+module.exports.getById = async (req, res) => {
+  try {
+    const category = await Category.findById(req.params.id);
+    res.status(200).json(category);
+  } catch (e) {
+    errorHandler(e);
+  }
 };
 
-module.exports.remove = (req, res) => {
+module.exports.remove = async (req, res) => {
+  try {
+    await Category.remove({ _id: req.params.id });
+    await Position.remove({ category: req.params.id });
 
+    res.status(200).json({
+      message: 'Category removed'
+    })
+
+  } catch (e) {
+    errorHandler(e);
+  }
 };
 
 module.exports.create = (req, res) => {
+  try {
 
+  } catch (e) {
+    errorHandler(e);
+  }
 };
 
 module.exports.update = (req, res) => {
+  try {
 
+  } catch (e) {
+    errorHandler(e);
+  }
 };
