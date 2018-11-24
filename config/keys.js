20539b7bf20566ad9900761f695c16f953185369
leskovitaliy <lesko.vitaliy90@gmail.com>
2018-11-24 14:18:04 +0200

    create logic login, add npm package jsonwebtoken

--- config/keys.js
+++ config/keys.js
@@ -1,3 +1,4 @@
 module.exports = {
-  mongoURL: 'mongodb://lesko.vitaliy:qwest9384402@ds247141.mlab.com:47141/crm'
+  mongoURL: 'mongodb://lesko.vitaliy:qwest9384402@ds247141.mlab.com:47141/crm',
+  jwt: 'dev-jwt'
 };
--- controllers/auth.js
+++ controllers/auth.js
@@ -1,20 +1,50 @@
 const bcrypt = require('bcryptjs');
+const jwt = require('jsonwebtoken');
 const User = require('../models/User');
+const keys = require('../config/keys');
 
 
-module.exports.login = (req, res) => {
-    res.status(200).json({
+module.exports.login = async (req, res) => {
+    /*res.status(200).json({
         login: {
             email: req.body.email,
             password: req.body.password
         }
-    })
+    })*/
+    const foundUser = await User.findOne({email: req.body.email});
+
+    if (foundUser) {
+        // check password
+        const isCheckPassword = bcrypt.compareSync(req.body.password, foundUser.password);
+
+        if (isCheckPassword) {
+            // generate token
+            const token = jwt.sign({
+                email: foundUser.email,
+                userId: foundUser._id
+            }, keys.jwt, {expiresIn: 60 * 60});
+
+            res.status(200).json({
+                token: `Bearer ${token}`
+            })
+
+        } else {
+            // Passwords do not match
+            res.status(401).json({
+                message: 'Wrong password.'
+            })
+        }
+    } else {
+        res.status(404).json({
+            message: `User with email: ${req.body.email} not found.`
+        });
+    }
 };
 
 module.exports.register = async (req, res) => {
-    const candidate = await User.findOne({ email: req.body.email });
+    const foundUser = await User.findOne({email: req.body.email});
 
-    if (candidate) {
+    if (foundUser) {
         // User exist return err
         res.status(409).json({
             message: 'This email exist'
