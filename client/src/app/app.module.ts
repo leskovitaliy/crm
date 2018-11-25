e6fa0463515a05ea52e0b9da6c44f5d5abde8c6d
leskovitaliy <lesko.vitaliy90@gmail.com>
2018-11-25 18:28:29 +0200

    [FE] add AuthService

--- client/src/app/app.module.ts
+++ client/src/app/app.module.ts
@@ -8,6 +8,7 @@ import { LoginPageComponent } from './login-page/login-page.component';
 import { AuthLayoutComponent } from './shared/layouts/auth-layout/auth-layout.component';
 import { SiteLayoutComponent } from './shared/layouts/site-layout/site-layout.component';
 import { RegisterPageComponent } from './register-page/register-page.component';
+import { HttpClientModule } from '@angular/common/http';
 
 @NgModule({
   declarations: [
@@ -21,7 +22,8 @@ import { RegisterPageComponent } from './register-page/register-page.component';
     BrowserModule,
     AppRoutingModule,
     FormsModule,
-    ReactiveFormsModule
+    ReactiveFormsModule,
+    HttpClientModule
   ],
   providers: [],
   bootstrap: [AppComponent]
--- client/src/app/login-page/login-page.component.ts
+++ client/src/app/login-page/login-page.component.ts
@@ -1,26 +1,57 @@
-import { Component, OnInit } from '@angular/core';
+import { Component, OnDestroy, OnInit } from '@angular/core';
 import { FormBuilder, FormGroup, Validators } from '@angular/forms';
+import { ActivatedRoute, Params, Router } from '@angular/router';
+
+import { AuthService } from '../shared/services/auth.service';
+import { Subscription } from 'rxjs';
 
 @Component({
   selector: 'app-login-page',
   templateUrl: './login-page.component.html',
   styleUrls: ['./login-page.component.scss']
 })
-export class LoginPageComponent implements OnInit {
+export class LoginPageComponent implements OnInit, OnDestroy {
 
   form: FormGroup;
+  sub$: Subscription;
 
-  constructor(private fb: FormBuilder) { }
+  constructor(private fb: FormBuilder,
+              private authService: AuthService,
+              private router: Router,
+              private aRoute: ActivatedRoute) { }
 
   ngOnInit() {
     this.form = this.fb.group({
       email: [null, [Validators.required, Validators.email]],
       password: [null, [Validators.required, Validators.minLength(6)]]
     });
+
+    this.aRoute.queryParams.subscribe((params: Params) => {
+      if (params['registered']) {
+        // Now you can login in app use your credential
+      } else if (params['accessDenied']) {
+        // please login in app
+      }
+    });
+  }
+
+  ngOnDestroy() {
+    if (this.sub$) {
+      this.sub$.unsubscribe();
+    }
   }
 
   onSubmit() {
+    this.form.disable();
 
+    this.sub$ = this.authService.login(this.form.value)
+      .subscribe(
+        () => this.router.navigate(['/overview']),
+        (err) => {
+          console.warn(err);
+          this.form.enable();
+        }
+      );
   }
 
 }
