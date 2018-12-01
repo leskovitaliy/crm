a3999c83e5f8b5430e638e417d6dd0154173ca66
leskovitaliy <lesko.vitaliy90@gmail.com>
2018-12-01 21:53:15 +0200

    add pages analytics, categories, order, history

--- client/src/app/app-routing.module.ts
+++ client/src/app/app-routing.module.ts
@@ -6,6 +6,10 @@ import { LoginPageComponent } from './login-page/login-page.component';
 import { RegisterPageComponent } from './register-page/register-page.component';
 import { AuthGuard } from './shared/guards/auth.guard';
 import { OverviewPageComponent } from './overview-page/overview-page.component';
+import { AnalyticsPageComponent } from './analytics-page/analytics-page.component';
+import { HistoryPageComponent } from './history-page/history-page.component';
+import { OrderPageComponent } from './order-page/order-page.component';
+import { CategoriesPageComponent } from './categories-page/categories-page.component';
 
 const routes: Routes = [
   {
@@ -31,10 +35,28 @@ const routes: Routes = [
     path: '',
     component: SiteLayoutComponent,
     canActivate: [AuthGuard],
-    children: [{
-      path: 'overview',
-      component: OverviewPageComponent
-    }]
+    children: [
+      {
+        path: 'overview',
+        component: OverviewPageComponent
+      },
+      {
+        path: 'analytics',
+        component: AnalyticsPageComponent
+      },
+      {
+        path: 'history',
+        component: HistoryPageComponent
+      },
+      {
+        path: 'order',
+        component: OrderPageComponent
+      },
+      {
+        path: 'categories',
+        component: CategoriesPageComponent
+      }
+    ]
   }
 ];
 
