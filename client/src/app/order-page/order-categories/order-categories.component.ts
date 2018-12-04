fe93d6f648537d53415eae9cf03872d27a222143
leskovitaliy <lesko.vitaliy90@gmail.com>
2018-12-04 22:32:21 +0200

    [FE] add order component, add order positions

---
+++ client/src/app/order-page/order-categories/order-categories.component.ts
@@ -0,0 +1,22 @@
+import { Component, OnInit } from '@angular/core';
+import { CategoriesService } from '../../shared/services/categories.service';
+import { Observable } from 'rxjs';
+import { ICategory } from '../../shared/interfaces';
+
+@Component({
+  selector: 'app-order-categories',
+  templateUrl: './order-categories.component.html',
+  styleUrls: ['./order-categories.component.scss']
+})
+export class OrderCategoriesComponent implements OnInit {
+
+  categories$: Observable<ICategory[]>;
+
+  constructor(private categoriesService: CategoriesService) {
+  }
+
+  ngOnInit() {
+    this.categories$ = this.categoriesService.get();
+  }
+
+}
--- client/src/app/order-page/order-page.component.ts
+++ client/src/app/order-page/order-page.component.ts
@@ -7,7 +7,8 @@ import { Component, OnInit } from '@angular/core';
 })
 export class OrderPageComponent implements OnInit {
 
-  constructor() { }
+  constructor() {
+  }
 
   ngOnInit() {
   }
