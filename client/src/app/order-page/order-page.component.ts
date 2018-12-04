1c001dff2ebba419948da55e0d7eb74eb4285035
leskovitaliy <lesko.vitaliy90@gmail.com>
2018-12-04 22:53:35 +0200

    [FE] add logic in order page

--- client/src/app/order-page/order-page.component.ts
+++ client/src/app/order-page/order-page.component.ts
@@ -1,16 +1,52 @@
-import { Component, OnInit } from '@angular/core';
+import { AfterViewInit, Component, ElementRef, OnDestroy, OnInit, ViewChild } from '@angular/core';
+import { NavigationEnd, Router } from '@angular/router';
+import { IMaterialInstance, MaterialService } from '../shared/services/material.service';
+import { OrderService } from './order.service';
 
 @Component({
   selector: 'app-order-page',
   templateUrl: './order-page.component.html',
-  styleUrls: ['./order-page.component.scss']
+  styleUrls: ['./order-page.component.scss'],
+  providers: [OrderService]
 })
-export class OrderPageComponent implements OnInit {
+export class OrderPageComponent implements OnInit, OnDestroy, AfterViewInit {
 
-  constructor() {
+  isRoot: boolean;
+  modal: IMaterialInstance;
+
+  @ViewChild('modal') modalRef: ElementRef;
+
+  constructor(private router: Router,
+              private orderService: OrderService) {
   }
 
   ngOnInit() {
+    this.isRoot = this.router.url === '/order';
+    this.router.events.subscribe(event => {
+      if (event instanceof NavigationEnd) {
+        this.isRoot = this.router.url === '/order';
+      }
+    });
+  }
+
+  ngAfterViewInit() {
+    this.modal = MaterialService.initModal(this.modalRef);
+  }
+
+  ngOnDestroy() {
+    this.modal.destroy();
+  }
+
+  open() {
+    this.modal.open();
+  }
+
+  cancel() {
+    this.modal.close();
+  }
+
+  submit() {
+    this.modal.close();
   }
 
 }
