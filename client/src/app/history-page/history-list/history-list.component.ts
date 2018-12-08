4ec26590dfeb44749945900e01ad206f25e59d11
leskovitaliy <lesko.vitaliy90@gmail.com>
2018-12-08 10:25:16 +0200

    [FE] add modal of order record

--- client/src/app/history-page/history-list/history-list.component.ts
+++ client/src/app/history-page/history-list/history-list.component.ts
@@ -1,17 +1,41 @@
-import { Component, Input, OnInit } from '@angular/core';
+import { AfterViewInit, Component, ElementRef, Input, OnDestroy, OnInit, ViewChild } from '@angular/core';
 import { IOrder } from '../../shared/interfaces';
+import { IMaterialInstance, MaterialService } from '../../shared/services/material.service';
 
 @Component({
   selector: 'app-history-list',
   templateUrl: './history-list.component.html',
   styleUrls: ['./history-list.component.scss']
 })
-export class HistoryListComponent implements OnInit {
+export class HistoryListComponent implements OnDestroy, AfterViewInit {
+  @ViewChild('modal') modalRef: ElementRef;
+
   @Input() orders: IOrder[];
 
-  constructor() { }
+  modal: IMaterialInstance;
+  selectedOrder: IOrder;
 
-  ngOnInit() {
+  ngAfterViewInit() {
+    this.modal = MaterialService.initModal(this.modalRef);
+  }
+
+  ngOnDestroy() {
+    this.modal.destroy();
+  }
+
+  computePrice(order: IOrder): number {
+    return order.list.reduce((total, item) => {
+      return total += item.quantity * item.cost;
+    }, 0);
+  }
+
+  selectOrder(order: IOrder) {
+    this.selectedOrder = order;
+    this.modal.open();
+  }
+
+  closeModal() {
+    this.modal.close();
   }
 
 }
